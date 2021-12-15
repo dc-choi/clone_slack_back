929526dea0771eee3abd26fc9d1d3c99ba442d8b
dcchoi <cheek0805@naver.com>
2021-12-15 14:46:22 +0900

    초기 개발 환경 구축

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+const { sequelize } = require('./models');
+
+sequelize.sync({ force: false })
+	.then(() => {
+		console.log('데이터베이스 연결 성공');
+	})
+	.catch((error) => {
+		console.error(error);
+	});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
