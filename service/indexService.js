00493801b276e596a25f964469280e52454968d8
dcchoi <cheek0805@naver.com>
2021-12-24 15:55:22 +0900

    원격에 올라간 코드가 양식이 불균형스러워서 다시 올림

--- service/indexService.js
+++ service/indexService.js
@@ -8,11 +8,9 @@ module.exports = {
     const ranNum = Math.floor(Math.random() * (max - min - 1)) + min;
 
     let emailParam = {
-      toEmail: email,     // 수신할 이메일
-
-      subject: `[slack_clone]인증 관련 메일 입니다`,   // 메일 제목
-
-      text: `오른쪽 숫자 6자리를 입력해주세요 : ${ranNum}`                // 메일 내용
+      toEmail: email, // 수신할 이메일
+      subject: `[slack_clone]인증 관련 메일 입니다`, // 메일 제목
+      text: `오른쪽 숫자 6자리를 입력해주세요 : ${ranNum}` // 메일 내용
     };
 
     mailer.sendGmail(emailParam);
